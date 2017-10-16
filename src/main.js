afda800c8b024ed8729ca22a6e06e200ce632758
willchen <1300546909@qq.com>
2017-10-16 18:37:06 +0800

    update info

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
 import hello from "script/hello"
 import 'style/main.scss'
 
-document.querySelector('#hello').innerHTML = `Hello ${hello.name}!`
+document.querySelector('#hello').innerHTML = `背景已转成base64 编码`
 let c = {c: 'c', d: 'd'};
+
+/*测试transform-object-rest-spread插件*/
 let test = {
     a: 'a',
     ...c
 }
- 
+console.log('transform-object-rest-spread', test)
 
+
+/*测试请求代理转发*/
 fetch('/api/list')
-.then(response => response.json() )
-.then(data => {
-    console.error(data);
+.then(response => {
+
+    response.json()
+}).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.log(error)
 })
 
 
